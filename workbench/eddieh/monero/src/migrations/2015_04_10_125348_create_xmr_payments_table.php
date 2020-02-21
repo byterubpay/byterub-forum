025841c7cada3a8c803e1e9e5035bceabc5fd6dc
majoricacoin <byterub@byterub.github.com>
2020-02-21 22:04:26 +0300

    master

--- workbench/eddieh/monero/src/migrations/2015_04_10_125348_create_xmr_payments_table.php
+++ workbench/eddieh/monero/src/migrations/2015_04_10_125348_create_xmr_payments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateXmrPaymentsTable extends Migration {
+class CreateBtrPaymentsTable extends Migration {
 
 	/**
 	 * Run the migrations.
